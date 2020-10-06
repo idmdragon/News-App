1b9e8dbfb1bbfd58c0a46e3167fc0599d446bb95
idmdragon <ilham.uklison@gmail.com>
2020-10-06 19:28:22 +0700

    breaking news

--- app/src/main/java/com/example/mynews/repository/NewsRepository.kt
+++ app/src/main/java/com/example/mynews/repository/NewsRepository.kt
@@ -1,7 +1,15 @@
 package com.example.mynews.repository
 
+import com.example.mynews.api.RetrofitInstance
 import com.example.mynews.db.ArticleDatabase
+import retrofit2.Retrofit
 
 class NewsRepository (
     val db : ArticleDatabase
-)
+){
+    suspend fun getBreakingNews(countryCode : String, pageNumber : Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode,pageNumber)
+
+    suspend fun searchNews(searchQuery : String, pageNumber: Int) =
+        RetrofitInstance.api.searchNews(searchQuery,pageNumber)
+}
--- app/src/main/java/com/example/mynews/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/mynews/ui/NewsViewModel.kt
@@ -1,10 +1,50 @@
 package com.example.mynews.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mynews.models.NewsResponse
 import com.example.mynews.repository.NewsRepository
+import com.example.mynews.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository : NewsRepository
 ) : ViewModel(){
 
+    val breakingNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1
+
+    val searchNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val searchNewsPage = 1
+
+    init {
+        getBreakingNews(countryCode = "us")
+    }
+
+    fun getBreakingNews(countryCode : String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode,breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse>{
+        if(response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse>{
+        if(response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
